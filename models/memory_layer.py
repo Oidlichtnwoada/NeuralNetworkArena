b30551d57ec44008b434c6e346e279b94bea9bfa
Hannes Brantner <hannes.brantner@hotmail.com>
2020-11-17 16:24:13 +0100

    updated memory layer code, which works now as a drop in replacement for an attention layer

--- models/memory_layer.py
+++ models/memory_layer.py
@@ -7,16 +7,16 @@ class MemoryCellWiring(ncp.wirings.Wiring):
         # input_dim = output_dim = dim
         self.dim = dim
         # allocate data structures for dim neuron pairs
-        super().__init__(units=self.dim * 2)
+        super().__init__(units=2 * self.dim)
         # the output dimension is half the state dimension of all neuron pairs
         self.set_output_dim(output_dim=self.dim)
         # create inhibitory recurrent connections to each neuron
         for src in range(self.units):
-            self.add_synapse(src, src, -1)
+            self.add_synapse(src, src, polarity=-1)
         # create inhibitory connections between each neuron pair
         for src in range(self.units // 2):
-            self.add_synapse(src, src + self.dim, -1)
-            self.add_synapse(src + self.dim, src, -1)
+            self.add_synapse(src, src + self.dim, polarity=-1)
+            self.add_synapse(src + self.dim, src, polarity=-1)
 
     def build(self, input_shape):
         super().build(input_shape)
@@ -42,22 +42,35 @@ class MemoryCell(tf.keras.layers.Layer):
 class MemoryLayer(tf.keras.layers.Layer):
     def __init__(self, dim, heads=1):
         super().__init__()
-        # save the dimension and the number of heads
-        self.dim = dim
-        self.heads = heads
-        # create a memory layer out of heads memory cells
-        self.memory_layer = [MemoryCell(dim) for _ in range(self.heads)]
-        # create a dense layer to merge all heads
-        self.dense_layer = tf.keras.layers.Dense(self.dim)
+        # create a memory layer out of heads memory cells with size twice dim because queries and keys are concatenated together
+        self.memory_layer = [MemoryCell(2 * dim) for _ in range(heads)]
+        # create a dense layer to merge all heads and the concatenated representation to size dim
+        self.dense_layer = tf.keras.layers.Dense(dim)
 
-    def compute_accumulated_input(self, inputs, input_index):
-        # the inputs are accumulated such that the last input to the RNN is the input at input_index
-        shifted_inputs = tf.roll(inputs, inputs.shape[1] - input_index - 1, axis=1)
-        # accumulate input via memory cell for each head and concatenate the outputs together
-        accumulated_input = tf.concat([memory_cell(shifted_inputs) for memory_cell in self.memory_layer], axis=-1)
-        # merge outputs from all heads via a dense layer and add a dimension for later concatenation
+    def compute_accumulated_representation(self, query, values):
+        # concatenate query at query_index to each value to create the input for the memory cells
+        duplicated_query = tf.repeat(query, values.shape[1], axis=1)
+        memory_cell_input = tf.concat([duplicated_query, values], axis=-1)
+        # accumulate information via memory cell for each head and concatenate the outputs together
+        accumulated_input = tf.concat([memory_cell(memory_cell_input) for memory_cell in self.memory_layer], axis=-1)
+        # merge outputs from all heads to size dim via a dense layer and add a dimension for later concatenation
         return tf.expand_dims(self.dense_layer(accumulated_input), axis=1)
 
     def call(self, inputs, **kwargs):
-        # compute an accumulated representation for all inputs
-        return tf.concat([self.compute_accumulated_input(inputs, input_index) for input_index in range(inputs.shape[1])], axis=1)
+        # split inputs tuple to the arguments
+        queries, values = inputs
+        # compute an accumulated representation for all queries
+        return tf.concat([self.compute_accumulated_representation(queries[:, query_index:query_index + 1, :], values) for query_index in range(queries.shape[1])], axis=1)
+
+
+class MemoryAccumulation(tf.keras.layers.Layer):
+    def __init__(self, dim, heads):
+        super().__init__()
+        # create the memory layer that should accumulate information
+        self.memory_layer = MemoryLayer(dim, heads)
+
+    def call(self, inputs, **kwargs):
+        # split inputs tuple to the arguments
+        queries, _, values, _ = inputs
+        # compute an accumulated representation using the memory layer
+        return self.memory_layer((queries, values))
