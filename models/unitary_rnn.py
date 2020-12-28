c826c0c9f0447ad89572bb9be1e0927963500202
Hannes Brantner <hannes.brantner@hotmail.com>
2020-12-28 18:41:21 +0100

    new unitary rnn, weights are being saved properly, but performance varies

--- models/unitary_rnn.py
+++ models/unitary_rnn.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import tensorflow as tf
 
@@ -75,12 +77,12 @@ def generate_index_fft(s):
                 list0.append(np.append(list1[index], list1[index] + 2 ** k))
             return list0
 
-    t = ind_s(int(tf.math.log(s / 2, 2)))
+    t = ind_s(int(math.log(s / 2, 2)))
     ind_exe = []
-    for i in range(int(tf.math.log(s, 2))):
+    for i in range(int(math.log(s, 2))):
         ind_exe.append(tf.constant(t[i]))
     ind_param = []
-    for i in range(int(tf.math.log(s, 2))):
+    for i in range(int(math.log(s, 2))):
         ind = np.array([])
         for j in range(2 ** i):
             ind = np.append(ind, np.array(range(0, s, 2 ** i)) + j).astype(np.int32)
@@ -120,33 +122,52 @@ class EUNNCell(tf.keras.layers.AbstractRNNCell):
         if self._capacity > self._num_units:
             raise ValueError("Do not set capacity larger than hidden size, it is redundant")
         if self._fft:
-            if tf.math.log(self._num_units, 2) % 1 != 0:
+            if math.log(self._num_units, 2) % 1 != 0:
                 raise ValueError("FFT style only supports power of 2 of hidden size")
         else:
             if self._num_units % 2 != 0:
                 raise ValueError("Tunable style only supports even number of hidden size")
             if self._capacity % 2 != 0:
                 raise ValueError("Tunable style only supports even number of capacity")
+
+    def build(self, input_shape):
+        input_matrix_init = tf.random_uniform_initializer(-0.01, 0.01)
+        inputs_size = input_shape[-1]
+        if self._cplex:
+            self.U_re = self.add_weight("U_re", [inputs_size, self._num_units], initializer=input_matrix_init)
+            self.U_im = self.add_weight("U_im", [inputs_size, self._num_units], initializer=input_matrix_init)
+        else:
+            self.U = self.add_weight("U", [inputs_size, self._num_units], initializer=input_matrix_init)
+        self.bias = self.add_weight("bias", [self._num_units], initializer=tf.constant_initializer())
         if self._fft:
-            self._capacity = int(tf.math.log(self._num_units, 2))
+            self._capacity = int(math.log(self._num_units, 2))
             self._v1, self._v2, self._ind, self._diag = self.fft_param(self._num_units, self._cplex)
         else:
             self._v1, self._v2, self._ind, self._diag = self.tunable_param(self._num_units, self._cplex, self._capacity)
+
+    @property
+    def state_size(self):
+        return self._num_units
+
+    @property
+    def output_size(self):
+        return self._num_units
+
+    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
         if self._cplex:
-            self.U_re = None
-            self.U_im = None
+            dtype = tf.complex64
         else:
-            self.U = None
-        self.bias = self.add_weight("modReLUBias", [self._num_units], initializer=tf.constant_initializer())
+            dtype = tf.float32
+        return tf.zeros((batch_size, self.state_size), dtype)
 
     def fft_param(self, num_units, cplex):
         phase_init = tf.random_uniform_initializer(-3.14, 3.14)
-        capacity = int(tf.math.log(num_units, 2))
-        theta = self.add_weight("theta", [capacity, num_units // 2], initializer=phase_init)
+        capacity = int(math.log(num_units, 2))
+        theta = self.add_weight("theta", [capacity, num_units // 2], initializer=phase_init, trainable=False)
         cos_theta = tf.cos(theta)
         sin_theta = tf.sin(theta)
         if cplex:
-            phi = self.add_weight("phi", [capacity, num_units // 2], initializer=phase_init)
+            phi = self.add_weight("phi", [capacity, num_units // 2], initializer=phase_init, trainable=False)
             cos_phi = tf.cos(phi)
             sin_phi = tf.sin(phi)
             cos_list_re = tf.concat([cos_theta, cos_theta * cos_phi], axis=1)
@@ -162,23 +183,22 @@ class EUNNCell(tf.keras.layers.AbstractRNNCell):
         v1 = tf.stack([tf.gather(cos_list[i, :], index_fft[i]) for i in range(capacity)])
         v2 = tf.stack([tf.gather(sin_list[i, :], index_fft[i]) for i in range(capacity)])
         if cplex:
-            omega = self.add_weight("omega", [num_units], initializer=phase_init)
+            omega = self.add_weight("omega", [num_units], initializer=phase_init, trainable=False)
             D = tf.complex(tf.cos(omega), tf.sin(omega))
         else:
             D = None
         diag = D
         return v1, v2, ind_exe, diag
 
-    @staticmethod
-    def tunable_param(num_units, cplex, capacity):
+    def tunable_param(self, num_units, cplex, capacity):
         capacity_A = int(capacity // 2)
         capacity_B = capacity - capacity_A
         phase_init = tf.random_uniform_initializer(-3.14, 3.14)
-        theta_A = tf.Variable(initial_value=phase_init([capacity_A, num_units // 2]), name="theta_A")
+        theta_A = self.add_weight("theta_A", [capacity_A, num_units // 2], initializer=phase_init, trainable=False)
         cos_theta_A = tf.cos(theta_A)
         sin_theta_A = tf.sin(theta_A)
         if cplex:
-            phi_A = tf.Variable(initial_value=phase_init([capacity_A, num_units // 2]), name="phi_A")
+            phi_A = self.add_weight("phi_A", [capacity_A, num_units // 2], initializer=phase_init, trainable=False)
             cos_phi_A = tf.cos(phi_A)
             sin_phi_A = tf.sin(phi_A)
             cos_list_A_re = tf.concat([cos_theta_A, cos_theta_A * cos_phi_A], axis=1)
@@ -190,22 +210,22 @@ class EUNNCell(tf.keras.layers.AbstractRNNCell):
         else:
             cos_list_A = tf.concat([cos_theta_A, cos_theta_A], axis=1)
             sin_list_A = tf.concat([sin_theta_A, -sin_theta_A], axis=1)
-        theta_B = tf.Variable(initial_value=phase_init([capacity_B, num_units // 2 - 1]), name="theta_B")
+        theta_B = self.add_weight("theta_B", [capacity_B, num_units // 2 - 1], initializer=phase_init, trainable=False)
         cos_theta_B = tf.cos(theta_B)
         sin_theta_B = tf.sin(theta_B)
         if cplex:
-            phi_B = tf.Variable(initial_value=phase_init([capacity_B, num_units // 2 - 1]), name="phi_B")
+            phi_B = self.add_weight("phi_B", [capacity_B, num_units // 2 - 1], initializer=phase_init, trainable=False)
             cos_phi_B = tf.cos(phi_B)
             sin_phi_B = tf.sin(phi_B)
             cos_list_B_re = tf.concat([tf.ones([capacity_B, 1]), cos_theta_B, cos_theta_B * cos_phi_B, tf.ones([capacity_B, 1])], axis=1)
             cos_list_B_im = tf.concat([tf.zeros([capacity_B, 1]), tf.zeros_like(theta_B), cos_theta_B * sin_phi_B, tf.zeros([capacity_B, 1])], axis=1)
-            sin_list_B_re = tf.concat([tf.zeros([capacity_B, 1]), sin_theta_B, - sin_theta_B * cos_phi_B, tf.zeros([capacity_B, 1])], axis=1)
-            sin_list_B_im = tf.concat([tf.zeros([capacity_B, 1]), tf.zeros_like(theta_B), - sin_theta_B * sin_phi_B, tf.zeros([capacity_B, 1])], axis=1)
+            sin_list_B_re = tf.concat([tf.zeros([capacity_B, 1]), sin_theta_B, -sin_theta_B * cos_phi_B, tf.zeros([capacity_B, 1])], axis=1)
+            sin_list_B_im = tf.concat([tf.zeros([capacity_B, 1]), tf.zeros_like(theta_B), -sin_theta_B * sin_phi_B, tf.zeros([capacity_B, 1])], axis=1)
             cos_list_B = tf.complex(cos_list_B_re, cos_list_B_im)
             sin_list_B = tf.complex(sin_list_B_re, sin_list_B_im)
         else:
             cos_list_B = tf.concat([tf.ones([capacity_B, 1]), cos_theta_B, cos_theta_B, tf.ones([capacity_B, 1])], axis=1)
-            sin_list_B = tf.concat([tf.zeros([capacity_B, 1]), sin_theta_B, - sin_theta_B, tf.zeros([capacity_B, 1])], axis=1)
+            sin_list_B = tf.concat([tf.zeros([capacity_B, 1]), sin_theta_B, -sin_theta_B, tf.zeros([capacity_B, 1])], axis=1)
         ind_exe, [index_A, index_B] = generate_index_tunable(num_units, capacity)
         diag_list_A = tf.gather(cos_list_A, index_A, axis=1)
         off_list_A = tf.gather(sin_list_A, index_A, axis=1)
@@ -214,32 +234,12 @@ class EUNNCell(tf.keras.layers.AbstractRNNCell):
         v1 = tf.reshape(tf.concat([diag_list_A, diag_list_B], axis=1), [capacity, num_units])
         v2 = tf.reshape(tf.concat([off_list_A, off_list_B], axis=1), [capacity, num_units])
         if cplex:
-            omega = tf.Variable(initial_value=phase_init([num_units]), name="omega")
+            omega = self.add_weight("omega", [num_units], initializer=phase_init, trainable=False)
             D = tf.complex(tf.cos(omega), tf.sin(omega))
         else:
             D = None
         diag = D
         return v1, v2, ind_exe, diag
-
-    @property
-    def state_size(self):
-        return self._num_units
-
-    @property
-    def output_size(self):
-        return self._num_units
-
-    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
-        return tf.ones((batch_size, self.state_size), dtype=tf.complex64)
-
-    def build(self, input_shape):
-        input_matrix_init = tf.random_uniform_initializer(-0.01, 0.01)
-        inputs_size = input_shape[-1]
-        if self._cplex:
-            self.U_re = self.add_weight("U_re", [inputs_size, self._num_units], initializer=input_matrix_init)
-            self.U_im = self.add_weight("U_im", [inputs_size, self._num_units], initializer=input_matrix_init)
-        else:
-            self.U = self.add_weight("U", [inputs_size, self._num_units], initializer=input_matrix_init)
 
     def loop(self, h):
         for i in range(self._capacity):
@@ -250,18 +250,14 @@ class EUNNCell(tf.keras.layers.AbstractRNNCell):
             h = h * self._diag
         return h
 
-    def call(self, inputs, state, scope=None):
-        # inputs to hidden
-        state = state[0]
+    def call(self, inputs, state):
         if self._cplex:
             inputs_re = tf.matmul(inputs, self.U_re)
             inputs_im = tf.matmul(inputs, self.U_im)
             inputs = tf.complex(inputs_re, inputs_im)
         else:
             inputs = tf.matmul(inputs, self.U)
-        # hidden to hidden
-        state = self.loop(state)
-        # activation
+        state = self.loop(state[0])
         output = self._activation((inputs + state), self.bias, self._cplex)
         return output, (output,)
 
