e5296b757026489d2a1ee22c0937a138b3b0e00e
Hannes Brantner <hannes.brantner@hotmail.com>
2021-01-04 17:42:37 +0100

    improved enhanced unitary rnn model

--- experiments/models/enhanced_unitary_rnn.py
+++ experiments/models/enhanced_unitary_rnn.py
@@ -4,7 +4,7 @@ import experiments.models.model_factory as model_factory
 
 
 def get_unitary_matrix(matrix):
-    return tf.linalg.expm(1j * (matrix + tf.linalg.adjoint(matrix)))
+    return tf.linalg.expm(matrix - tf.linalg.adjoint(matrix))
 
 
 def modrelu(x, bias):
@@ -19,13 +19,10 @@ class EnhancedUnitaryRNN(tf.keras.layers.AbstractRNNCell):
         self.output_size_value = output_size
         self.real_state_matrix = self.add_weight('real_state_matrix', (self.state_size, self.state_size), tf.float32, tf.keras.initializers.Identity())
         self.imag_state_matrix = self.add_weight('imag_state_matrix', (self.state_size, self.state_size), tf.float32, tf.keras.initializers.Constant())
-        self.real_step_matrix = self.add_weight('real_step_matrix', (self.state_size, self.state_size), tf.float32, tf.keras.initializers.Identity())
-        self.imag_step_matrix = self.add_weight('imag_step_matrix', (self.state_size, self.state_size), tf.float32, tf.keras.initializers.Constant())
-        self.first_bias = self.add_weight('first_bias', (self.state_size, 1), tf.float32, tf.keras.initializers.Constant())
-        self.second_bias = self.add_weight('second_bias', (self.state_size, 1), tf.float32, tf.keras.initializers.Constant())
+        self.real_initial_state = self.add_weight('real_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant(1 / self.state_size ** 0.5))
+        self.imag_initial_state = self.add_weight('imag_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant())
+        self.bias = self.add_weight('bias', (self.state_size, 1), tf.float32, tf.keras.initializers.Constant())
         self.output_layer = tf.keras.layers.Dense(self.output_size)
-        self.real_initial_state = None
-        self.imag_initial_state = None
         self.real_input_matrix = None
         self.imag_input_matrix = None
 
@@ -42,24 +39,20 @@ class EnhancedUnitaryRNN(tf.keras.layers.AbstractRNNCell):
 
     def build(self, input_shape):
         inputs_size = model_factory.get_concat_input_shape(input_shape)
-        self.real_initial_state = self.add_weight('real_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant(1))
-        self.imag_initial_state = self.add_weight('imag_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant())
-        self.real_input_matrix = self.add_weight('real_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.Identity())
-        self.imag_input_matrix = self.add_weight('imag_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.Identity())
+        self.real_input_matrix = self.add_weight('real_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.GlorotUniform())
+        self.imag_input_matrix = self.add_weight('imag_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.GlorotUniform())
 
     def call(self, inputs, states):
         inputs = model_factory.get_concat_inputs(inputs)
         state_matrix = get_unitary_matrix(tf.complex(self.real_state_matrix, self.imag_state_matrix))
-        step_matrix = get_unitary_matrix(tf.complex(self.real_step_matrix, self.imag_step_matrix))
         input_matrix = tf.complex(self.real_input_matrix, self.imag_input_matrix)
         time_domain_inputs = tf.cast(inputs, tf.complex64)
         frequency_domain_inputs = tf.signal.fft(time_domain_inputs)
         input_parts = tf.matmul(input_matrix, tf.concat((time_domain_inputs, frequency_domain_inputs), -1)[..., tf.newaxis])
         state_parts = tf.matmul(state_matrix, states[0][..., tf.newaxis])
-        preliminary_next_states = modrelu(state_parts + input_parts, self.first_bias)
-        next_states = tf.squeeze(modrelu(tf.matmul(step_matrix, preliminary_next_states), self.second_bias), -1)
-        output = self.output_layer(tf.concat((tf.math.real(next_states), tf.math.imag(next_states)), -1))
-        return output, (next_states,)
+        next_states = tf.squeeze(modrelu(state_parts + input_parts, self.bias), -1)
+        outputs = self.output_layer(tf.concat((tf.math.real(next_states), tf.math.imag(next_states)), -1))
+        return outputs, (next_states,)
 
     def get_config(self):
         config = super().get_config().copy()
