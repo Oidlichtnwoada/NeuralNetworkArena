cf8ab2c8ab6d7e345939f8c441c13537d490a8a3
Hannes Brantner <hannes.brantner@hotmail.com>
2021-01-27 12:08:18 +0100

    improved enhanced unitary rnn initialization

--- experiments/models/enhanced_unitary_rnn.py
+++ experiments/models/enhanced_unitary_rnn.py
@@ -17,14 +17,17 @@ def modrelu(x, bias):
 
 @tf.keras.utils.register_keras_serializable()
 class EnhancedUnitaryRNN(tf.keras.layers.AbstractRNNCell):
-    def __init__(self, state_size, output_size, **kwargs):
+    def __init__(self, state_size, output_size, use_fft=False, trainable_initial_state=False, **kwargs):
         super().__init__(**kwargs)
         self.state_size_value = state_size
         self.output_size_value = output_size
-        self.real_state_vector = self.add_weight('real_state_vector', (self.state_size * (self.state_size + 1) // 2,), tf.float32, tf.keras.initializers.Constant())
-        self.imag_state_vector = self.add_weight('imag_state_vector', (self.state_size * (self.state_size + 1) // 2,), tf.float32, tf.keras.initializers.Constant())
-        self.real_initial_state = self.add_weight('real_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant(1 / self.state_size ** 0.5))
-        self.imag_initial_state = self.add_weight('imag_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant())
+        self.use_fft = use_fft
+        self.trainable_initial_state = trainable_initial_state
+        self.initializer = tf.random_uniform_initializer(-1E-3, 1E-3)
+        self.real_state_vector = self.add_weight('real_state_vector', (self.state_size * (self.state_size + 1) // 2,), tf.float32, self.initializer)
+        self.imag_state_vector = self.add_weight('imag_state_vector', (self.state_size * (self.state_size + 1) // 2,), tf.float32, self.initializer)
+        self.real_initial_state = self.add_weight('real_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant(), trainable=self.trainable_initial_state)
+        self.imag_initial_state = self.add_weight('imag_initial_state', (1, self.state_size), tf.float32, tf.keras.initializers.Constant(), trainable=self.trainable_initial_state)
         self.bias = self.add_weight('bias', (self.state_size, 1), tf.float32, tf.keras.initializers.Constant())
         self.output_layer = tf.keras.layers.Dense(self.output_size)
         self.real_input_matrix = None
@@ -43,16 +46,21 @@ class EnhancedUnitaryRNN(tf.keras.layers.AbstractRNNCell):
 
     def build(self, input_shape):
         inputs_size = model_factory.get_concat_input_shape(input_shape)
-        self.real_input_matrix = self.add_weight('real_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.GlorotUniform())
-        self.imag_input_matrix = self.add_weight('imag_input_matrix', (self.state_size, 2 * inputs_size), tf.float32, tf.keras.initializers.Constant())
+        factor = 2 if self.use_fft else 1
+        self.real_input_matrix = self.add_weight('real_input_matrix', (self.state_size, factor * inputs_size), tf.float32, self.initializer)
+        self.imag_input_matrix = self.add_weight('imag_input_matrix', (self.state_size, factor * inputs_size), tf.float32, self.initializer)
 
     def call(self, inputs, states):
         inputs = model_factory.get_concat_inputs(inputs)
         state_matrix = get_unitary_matrix(tf.complex(self.real_state_vector, self.imag_state_vector))
         input_matrix = tf.complex(self.real_input_matrix, self.imag_input_matrix)
         time_domain_inputs = tf.cast(inputs, tf.complex64)
-        frequency_domain_inputs = tf.signal.fft(time_domain_inputs)
-        input_parts = tf.matmul(input_matrix, tf.concat((time_domain_inputs, frequency_domain_inputs), -1)[..., tf.newaxis])
+        if self.use_fft:
+            frequency_domain_inputs = tf.signal.fft(time_domain_inputs)
+            augmented_inputs = tf.concat((time_domain_inputs, frequency_domain_inputs), -1)
+        else:
+            augmented_inputs = time_domain_inputs
+        input_parts = tf.matmul(input_matrix, augmented_inputs[..., tf.newaxis])
         state_parts = tf.matmul(state_matrix, states[0][..., tf.newaxis])
         next_states = tf.squeeze(modrelu(state_parts + input_parts, self.bias), -1)
         outputs = self.output_layer(tf.concat((tf.math.real(next_states), tf.math.imag(next_states)), -1))
@@ -63,5 +71,7 @@ class EnhancedUnitaryRNN(tf.keras.layers.AbstractRNNCell):
         config.update({
             'state_size': self.state_size,
             'output_size': self.output_size,
+            'use_fft': self.use_fft,
+            'trainable_initial_state': self.trainable_initial_state
         })
         return config
