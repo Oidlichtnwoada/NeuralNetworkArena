c34c8dc7480c773f2377aa38769c48474f6943de
Hannes Brantner <hannes.brantner@hotmail.com>
2021-01-31 11:21:00 +0100

    improved benchmark script that starts all benchmarks

--- run_all_benchmarks_and_models.py
+++ run_all_benchmarks_and_models.py
@@ -1,14 +1,23 @@
+import argparse
 import os
 import subprocess
 
 import experiments.benchmarks.benchmark as benchmark
 import experiments.models.model_factory as model_factory
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--cuda_visible_devices', default='', type=str)
+parser.add_argument('--result_folder_name', default='results', type=str)
+parser.add_argument('--python_executable_name', default='python3.8', type=str)
+args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda_visible_devices
+
 for benchmark_name in benchmark.BENCHMARK_NAMES:
     for model_argument in model_factory.MODEL_ARGUMENTS:
-        if not os.path.exists(os.path.join(os.path.curdir, 'results', benchmark_name, model_argument, 'training.csv')):
+        if not os.path.exists(os.path.join(os.path.curdir, args.result_folder_name, benchmark_name, model_argument, 'training.csv')):
             if benchmark_name == 'cell' and model_argument != 'memory_cell':
                 continue
             if benchmark_name != 'cell' and model_argument == 'memory_cell':
                 continue
-            subprocess.run(f'python3.8 -m experiments.benchmarks.{benchmark_name}_benchmark --model {model_argument}')
+            subprocess.run(f'{args.python_executable_name} -m experiments.benchmarks.{benchmark_name}_benchmark --model {model_argument} --result_folder_name {args.result_folder_name}')
